3572288f8c2ba09e8579ddf37e3ed92408aaf7f5
NomadicDaggy <dagsagrinbergs@gmail.com>
2022-08-20 18:10:54 +0300

    Add pg healthchecks to run containers from sh

--- dev_run.sh
+++ dev_run.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 docker-compose down -v
 docker-compose up -d --build
 docker-compose exec web python manage.py create_db
--- prod_run.sh
+++ prod_run.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 docker-compose -f docker-compose.prod.yml down -v
 docker-compose -f docker-compose.prod.yml up -d --build
 docker-compose -f docker-compose.prod.yml exec web python manage.py create_db
