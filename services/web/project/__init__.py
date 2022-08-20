2cdc852167a49ed20a48fb79d8fd5da222112bd8
NomadicDaggy <dagsagrinbergs@gmail.com>
2022-08-20 16:48:28 +0300

    add file uploader to dev

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,5 +1,14 @@
-import json
-from flask import Flask, jsonify, send_from_directory
+import os
+
+from werkzeug.utils import secure_filename
+from flask import (
+    Flask,
+    jsonify,
+    send_from_directory,
+    request,
+    redirect,
+    url_for
+)
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -27,3 +36,22 @@ def hello_world():
 @app.route("/static/<path:filename>")
 def staticfiles(filename):
     return send_from_directory(app.config["STATIC_FOLDER"], filename)
+
+
+@app.route("/media/<path:filename>")
+def mediafiles(filename):
+    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        file = request.files["file"]
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
+    return """
+    <!doctype html>
+    <title>upload new File</title>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file><input type=submit value=Upload>
+    </form>
+    """
