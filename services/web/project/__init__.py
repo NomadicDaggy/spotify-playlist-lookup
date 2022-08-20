2585abddc4e3d16e1a4fb51b36ccbdc17fef3f33
NomadicDaggy <dagsagrinbergs@gmail.com>
2022-08-20 16:30:27 +0300

    add static file serving to dev env

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -22,3 +22,8 @@ class User(db.Model):
 @app.route("/")
 def hello_world():
     return jsonify(hello="world")
+
+
+@app.route("/static/<path:filename>")
+def staticfiles(filename):
+    return send_from_directory(app.config["STATIC_FOLDER"], filename)
--- services/web/project/config.py
+++ services/web/project/config.py
@@ -7,3 +7,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
