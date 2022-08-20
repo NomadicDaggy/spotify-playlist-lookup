4f1402da20ecbf0115b4babf81d2584026d87548
NomadicDaggy <dagsagrinbergs@gmail.com>
2022-08-20 16:48:28 +0300

    add file uploader to dev

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -8,3 +8,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
+    MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/project/media"
