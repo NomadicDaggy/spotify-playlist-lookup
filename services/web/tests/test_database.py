d3c44a61f53673a82d429292ec606ec86de49899
NomadicDaggy <dagsagrinbergs@gmail.com>
2022-08-27 10:33:03 +0300

    add test

--- services/web/tests/test_database.py
+++ services/web/tests/test_database.py
@@ -1,5 +1,12 @@
 from app.models import User
 
 
-def test_db_empty():
+def test_insert_user(session):
+    u = User(email="test@test.com", username="tester", active=True)
+    session.add(u)
+    session.commit()
+    assert User.query.count() == 1
+
+
+def test_no_users():
     assert User.query.count() == 0
