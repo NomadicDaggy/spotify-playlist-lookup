9955459cd657fe11e00d6afa971f48301154ff8f
NomadicDaggy <dagsagrinbergs@gmail.com>
2022-08-27 12:42:05 +0300

    add realtionships

--- services/web/tests/test_database.py
+++ services/web/tests/test_database.py
@@ -1,4 +1,4 @@
-from app.models import User
+from app.models import User, Playlist, Track, PlaylistTrack
 
 
 def test_insert_user(session):
@@ -10,3 +10,31 @@ def test_insert_user(session):
 
 def test_no_users():
     assert User.query.count() == 0
+
+
+def test_playlist_tracks(session):
+
+    playlists = [Playlist(spotify_id="0" * 22), Playlist(spotify_id="1" * 22)]
+    session.add_all(playlists)
+
+    tracks = [
+        Track(spotify_id="0" * 22, name="0"),
+        Track(spotify_id="1" * 22, name="1"),
+        Track(spotify_id="2" * 22, name="2"),
+        Track(spotify_id="3" * 22, name="3"),
+    ]
+    session.add_all(tracks)
+
+    session.commit()
+
+    playlist_tracks = [
+        PlaylistTrack(playlist_id=1, track_id=1),
+        PlaylistTrack(playlist_id=1, track_id=2),
+        PlaylistTrack(playlist_id=2, track_id=3),
+        PlaylistTrack(playlist_id=2, track_id=4),
+    ]
+    session.add_all(playlist_tracks)
+
+    session.commit()
+
+    assert PlaylistTrack.query.count() == 4
