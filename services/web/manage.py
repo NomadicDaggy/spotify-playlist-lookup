6039c75c819c9751b7f968468eac51a157f45658
NomadicDaggy <dagsagrinbergs@gmail.com>
2022-08-28 21:59:35 +0300

    add one playlist on startup

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,6 +1,7 @@
 from flask.cli import FlaskGroup
 
-from app.models import User, db
+from app.models import User, db, insert_playlists_tracks
+from app.api_data_import import MaterializedPlaylist
 from app import create_app
 
 
@@ -23,6 +24,10 @@ def seed_db():
     user.set_password("admin")
     db.session.add(user)
     db.session.commit()
+
+    mp = MaterializedPlaylist("50CP5OXFyr6qmCX8wZoum5")
+    playlist_data = mp.get_data()
+    insert_playlists_tracks(playlist_data)
 
 
 if __name__ == "__main__":
