7c855fe1fe9a22d8749eccd0d2f1209604ad3080
NomadicDaggy <dagsagrinbergs@gmail.com>
2022-08-31 11:36:08 +0300

    remove unneeded test

--- services/web/tests/test_database.py
+++ services/web/tests/test_database.py
@@ -14,31 +14,6 @@ def test_no_users():
     assert User.query.count() == 0
 
 
-def test_manually_add_playlist_tracks(session):
-
-    playlists = [Playlist(spotify_id="0" * 22), Playlist(spotify_id="1" * 22)]
-    session.add_all(playlists)
-    tracks = [
-        Track(spotify_id="0" * 22, name="0"),
-        Track(spotify_id="1" * 22, name="1"),
-        Track(spotify_id="2" * 22, name="2"),
-        Track(spotify_id="3" * 22, name="3"),
-    ]
-    session.add_all(tracks)
-    session.commit()
-
-    playlist_tracks = [
-        PlaylistTrack(playlist_id=1, track_id=1),
-        PlaylistTrack(playlist_id=1, track_id=2),
-        PlaylistTrack(playlist_id=2, track_id=3),
-        PlaylistTrack(playlist_id=2, track_id=4),
-    ]
-    session.add_all(playlist_tracks)
-    session.commit()
-
-    assert PlaylistTrack.query.count() == 4
-
-
 p_unique = {
     "id": "0" * 22,
     "tracks": [
