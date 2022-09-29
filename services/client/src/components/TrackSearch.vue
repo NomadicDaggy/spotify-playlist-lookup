acdc59ebdd5099057abc58f7ad18fd41997a7af6
NomadicDaggy <dagsagrinbergs@gmail.com>
2022-09-29 11:44:24 +0300

    show found track names

--- services/client/src/components/TrackSearch.vue
+++ services/client/src/components/TrackSearch.vue
@@ -1,33 +1,44 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import type { Ref } from "vue";
 import axios from "axios";
 
 const searchTerm = ref("");
 const statusText = ref("Enter text to search playlists by tracks they contain");
+const tracks = ref();
 
 const fetchTracks = () => {
   axios
     .get("http://localhost:1337/api/v1/tracks?name=" + searchTerm.value)
     .then((response) => {
-      console.log(response.data);
       let foundTrackCount = response.data["count"];
       statusText.value = `Found ${foundTrackCount} tracks.`;
+
+      tracks.value = response.data["tracks"];
     })
     .catch((error) => {
       console.log(error);
     });
 };
 
-function getTracks() {
-  console.log(searchTerm.value);
-  fetchTracks();
+function selectTrack(track: Ref) {
+  console.log(track);
 }
 </script>
 
 <template>
   <div class="track-search-container">
-    <input v-model="searchTerm" @keyup.enter="getTracks" />
+    <input v-model="searchTerm" @keyup.enter="fetchTracks" />
     <span>{{ statusText }}</span>
+    <div id="tracks-container">
+      <div
+        v-for="track in tracks"
+        :key="track.spotify_id"
+        @click="selectTrack(track)"
+      >
+        {{ track.name }}
+      </div>
+    </div>
   </div>
 </template>
 
